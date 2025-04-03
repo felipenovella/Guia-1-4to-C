04c45a45173bb7a0e827d2adaa5d66cfefa47c56
felipenovella <49439231@est.ort.edu.ar>
2025-04-03 11:40:34 -0300

    upa picante

---
+++ Assets/Scripts/Calcules.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calcules : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    public int resultado;
+
+    void Start()
+    {
+        resultado = num1 + num2;
+        Debug.Log(resultado);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
