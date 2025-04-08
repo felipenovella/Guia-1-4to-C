4323291babdd074884b178ec962f556c04fa2465
felipenovella <49439231@est.ort.edu.ar>
2025-04-08 09:01:27 -0300

    examen hecho picante la cosa

---
+++ Assets/Scripts/conversionDeMoneda.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class conversionDeMoneda : MonoBehaviour
+{
+    public float MontoAConvertir;
+    public string codigoMoneda;
+    float montoConvertido;
+    float CotizacionMonedaElegida;
+
+    void Start()
+    {
+        if (MontoAConvertir < 1000)
+        {
+            Debug.Log("El monto minimo es $1000");
+            return;
+        }
+        if (codigoMoneda == "D")
+        {
+            CotizacionMonedaElegida = 1134.5f;
+
+        } else if(codigoMoneda == "E")
+        {
+            CotizacionMonedaElegida = 1397;
+
+        } else if(codigoMoneda == "R")
+        {
+            CotizacionMonedaElegida = 211.8f;
+
+        }
+        else
+        {
+            Debug.Log("Opcion de monda extranjera no valida");
+        }
+
+        montoConvertido = MontoAConvertir / CotizacionMonedaElegida;
+
+        Debug.Log("El monto convertido es de " + montoConvertido);
+    }
+
+    void Update()
+    {
+        
+    }
+} 
